9741dd839c62536334d85ca25755b8a76a67d43a
unknown <dheepi117@gmail.com>
2022-12-17 12:36:04 +0530

    add problem statement.

---
+++ js/script.js
@@ -0,0 +1,43 @@
+a=32415;
+sum=0
+while (a>0)
+{
+	var rem=a%10;       
+	sum=rem+(sum*10)
+	a=(a-rem)/10;
+	console.log(rem)
+}
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
